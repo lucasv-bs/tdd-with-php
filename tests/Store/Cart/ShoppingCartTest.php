423f26a8e28c930e5381ad7f1ed9013a5ae9f41d
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-10-13 12:55:40 -0300

    Refactor the ShoppingCartTest class

--- tests/Store/Cart/ShoppingCartTest.php
+++ tests/Store/Cart/ShoppingCartTest.php
@@ -9,11 +9,18 @@ use TDD\Store\Cart\ShoppingCart,
 
 class ShoppingCartTest extends TestCase
 {
+    private $cart;
+
+    protected function setUp(): void
+    {
+        $this->cart = new ShoppingCart();
+        parent::setUp();
+    }
+
+
     public function testMustReturnZeroIfCartIsEmpty()
     {
-        $cart = new ShoppingCart();
-
-        $value = $cart->maxValue();
+        $value = $this->cart->maxValue();
 
         $this->assertEqualsWithDelta(0, $value, 0.0001);
     }
@@ -21,10 +28,9 @@ class ShoppingCartTest extends TestCase
 
     public function testMustReturnItemValueIfCartHas1Element()
     {
-        $cart = new ShoppingCart();
-        $cart->add(new Product("Geladeira", 900.00, 1));
+        $this->cart->add(new Product("Geladeira", 900.00, 1));
 
-        $value = $cart->maxValue();
+        $value = $this->cart->maxValue();
 
         $this->assertEqualsWithDelta(900.00, $value, 0.0001);
     }
@@ -32,6 +38,14 @@ class ShoppingCartTest extends TestCase
 
     public function testMustReturnMaxValueIfCartWithManyElements()
     {
+        $this->cart->add(new Product("Geladeira", 900.00, 1));
+        $this->cart->add(new Product("Fogão", 1500.00, 1));
+        $this->cart->add(new Product("Máquina de lavar", 750.00, 1));
+
+        $value = $this->cart->maxValue();
+
+        $this->assertEqualsWithDelta(1500.00, $value, 0.0001);
+    }
 
 
     public function testDemonstratesUsingTheTestDataBuilder()
